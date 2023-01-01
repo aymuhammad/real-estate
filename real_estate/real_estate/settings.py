fc3fe73fe55b07341c0d7ffd06f9e224f21edcc9
Auwal Yahaya <auwalyah@yahoo.com>
2023-01-01 12:26:53 +0100

    update templates

--- real_estate/real_estate/settings.py
+++ real_estate/real_estate/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "real_estate.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [ BASE_DIR / "templates"],
+        "DIRS": [ BASE_DIR / "real_estate/templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
