6833fe21df2c0867284fb9fd7dda67688c4d9eff
Auwal Yahaya <auwalyah@yahoo.com>
2023-01-04 12:44:13 +0100

    set media url

--- real_estate/real_estate/urls.py
+++ real_estate/real_estate/urls.py
@@ -1,6 +1,8 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from listings.views import listing_list, listing_retrieve, listing_create, listing_update, listing_delete
+from listings.views import (listing_create, listing_delete, listing_list, listing_retrieve, listing_update)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,3 +12,6 @@ urlpatterns = [
     path('listings/<pk>/edit/', listing_update),
     path('listings/<pk>/delete/', listing_delete),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
